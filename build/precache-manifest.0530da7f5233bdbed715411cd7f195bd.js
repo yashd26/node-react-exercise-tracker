45ca16e9c77d650027eebb0bb1923dc7f4408892
yashd26 <yashd026@gmail.com>
2023-06-25 13:44:10 +0530

    #8

---
+++ build/precache-manifest.0530da7f5233bdbed715411cd7f195bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854b38d11254a7e367abda59c40e5008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e5e6fb92c11fe1803ac",
+    "url": "/static/css/2.75cbde76.chunk.css"
+  },
+  {
+    "revision": "4e5e6fb92c11fe1803ac",
+    "url": "/static/js/2.71014f87.chunk.js"
+  },
+  {
+    "revision": "cab6952e715c3841e6db",
+    "url": "/static/js/main.b233a673.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
